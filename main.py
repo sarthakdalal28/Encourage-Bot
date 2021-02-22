81dc05d6ba8aa2da5f9d511b52274b74d6761d4b
Sarthak Dalal <sarthak.dalal@gmail.com>
2021-02-22 07:50:34 +0000

    Bot responds with motivational messages when user send a sad message

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import discord
 import os
+import requests
+import json
+import random
 
 client = discord.Client()
 
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"] #list of sad words
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person!"
+] #list of motivation sentences
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random") #Return a random quote from this API
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+  
+
 @client.event #register an event
 async def on_ready():  #When bot is ready
-  print('We have logged in as {0.user}.format(client)') #0 gets replaced by client 
+  print('We have logged in as {0.user}'.format(client)) #0 gets replaced by client 
 
 @client.event
 async def on_message(message): #Trigger when a message is received
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!') #Bot will respond to $hello
+  msg = message.content  
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote) #Bot will respond to $hello
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv('TOKEN')) #run the bot
